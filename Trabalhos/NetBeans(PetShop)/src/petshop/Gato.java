f4186a6407106f8caa7f338603f5bb13b5a2cf4c
felipe-franca69420 <82882257+felipe-franca69420@users.noreply.github.com>
2022-09-22 21:19:04 -0300

    Create Gato.java

---
+++ Trabalhos/NetBeans(PetShop)/src/petshop/Gato.java
@@ -0,0 +1,82 @@
+package petshop;
+
+public class Gato extends Animal{
+    
+    int nivelfofo;
+    boolean castrado;
+    String alergias;
+
+    public int getNivelfofo() {
+        return nivelfofo;
+    }
+
+    public void setNivelfofo(int fofo) {
+        this.nivelfofo = fofo;
+    }
+
+    public boolean isCastrado() {
+        return castrado;
+    }
+
+    public void setCastrado(boolean castrado) {
+        this.castrado = castrado;
+    }
+
+    public String getAlergias() {
+        return alergias;
+    }
+
+    public void setAlergias(String alergias) {
+        this.alergias = alergias;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor_pelagem() {
+        return cor_pelagem;
+    }
+
+    public void setCor_pelagem(String cor_pelagem) {
+        this.cor_pelagem = cor_pelagem;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+    
+    
+}
