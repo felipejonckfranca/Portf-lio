0fa43dd023facca304b43414dc192a2f29296293
Felipe França <82882257+felipejonckfranca@users.noreply.github.com>
2023-03-16 20:46:05 -0300

    Create Soma_valores_entre.java
    Somar todos os valores entre dois numeros incluindo eles

---
+++ Trabalhos/Programação de soluções computacionais/Soma_valores_entre.java
@@ -0,0 +1,17 @@
+public class Exercicio23 {
+    public static void main(String[] args) {
+        int inicio = Integer.parseInt(JOptionPane.showInputDialog("digite o primeiro numero"));
+        int finale = Integer.parseInt(JOptionPane.showInputDialog("digite o ultimo numero"));
+       
+        int i = inicio;
+        int soma = 0;
+       
+        while(inicio<=i&&i<=finale){
+            soma+=i;
+            i++;
+        }
+        JOptionPane.showMessageDialog(null, soma);
+       
+    }
+
+}
