19de064dbad8aa906912f20df858a790a023b67f
Felipe França <82882257+felipejonckfranca@users.noreply.github.com>
2023-03-17 20:36:06 -0300

    Create MODEL_teste_criacao_produtos.java

---
+++ Trabalhos/Programação de soluções computacionais/MODEL_teste_criacao_produtos.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author 10723113325
+ */
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int estoque;
+
+    public Produto() {
+    }
+
+    public Produto(int id, String nome, String descricao, double preco, int estoque) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+    
+    
+}
