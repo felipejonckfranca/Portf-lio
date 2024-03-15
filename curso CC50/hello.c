ce2949275567cc567ed850d95188442fb61b54c7
Felipe França <82882257+felipejonckfranca@users.noreply.github.com>
2024-03-14 21:21:40 -0300

    Delete curso CC50 directory

--- curso CC50/hello.c
+++
@@ -1,9 +0,0 @@
-#include <cs50.h>
-#include <stdio.h>
-//Nome do criador: Felipe Jonck França. Função: Perguntar o nome do usuario e fazer uma saudação
-
-int main(void)
-{
-    string nome = get_string("Qual é o seu nome?\n");
-    printf("Olá, %s \n", nome);
-    }
--- curso CC50/python/ola.py
+++
@@ -1,4 +0,0 @@
-from cs50 import get_string
-
-answer = get_string("qual é o seu nome? ")
-print("Olá, " + answer)
