98c52a685370f60985456d22747f79f46bdc220e
Felipe França <82882257+felipejonckfranca@users.noreply.github.com>
2024-09-30 20:32:33 -0300

    Create Numeros Pares.java

---
+++ Numeros Pares.java
@@ -0,0 +1,13 @@
+//Felipe Jonck França
+
+public class Exercicio24 {
+    public static void main(String[] args) {
+        String mensagem="";
+        for (int i =33; i < 57; i++) {
+            if(i%2!=0){
+                mensagem+=i+"\n";
+            }
+        }
+        JOptionPane.showMessageDialog(null, mensagem);
+    }
+}
