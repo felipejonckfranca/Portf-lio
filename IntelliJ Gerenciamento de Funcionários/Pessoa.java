8ddc0a5cfa0717f6534bd21e9aa4e0a7c8836d48
Felipe França <82882257+felipejonckfranca@users.noreply.github.com>
2024-09-30 20:14:43 -0300

    Create Pessoa.java

---
+++ IntelliJ Gerenciamento de Funcionários/Pessoa.java
@@ -0,0 +1,35 @@
+//Felipe Jonck França, Joao Pedro Lourenco David
+//Trabalho 1 - 02/09/2024
+
+public abstract class Pessoa {
+    String nome, telefone;
+
+    public Pessoa(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+}
