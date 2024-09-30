e1e2dc841bba181d8db064f29b5e5f2392afc0f1
Felipe França <82882257+felipejonckfranca@users.noreply.github.com>
2024-09-30 20:38:33 -0300

    Create Model.java

---
+++ Criação de produtos/Model.java
@@ -0,0 +1,63 @@
+//Felipe Jonck França
+package Model;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int estoque;
+
+    public Produto() {
+    }
+
+    public Produto(int id, String nome, String descricao, double preco, int estoque) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+    
+    
+}
